aab25eedeb764864ff3bba4864ea60dbc5302450
shawnmcclure <shawn850mcclure@gmail.com>
2023-10-09 18:17:26 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  //console.log(form)
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    //console.log(e.target.new-task-description)
+    buildToDO(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
+function buildToDO(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener("click",handleDelete)
+  li.textContent = `${todo}`
+  li.appendChild(btn)
+  //console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
